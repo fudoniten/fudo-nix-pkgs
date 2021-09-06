8ff5dfa3481c1e625f612e0b01a899dd1c37420b
niten <niten@fudo.org>
2021-09-05 23:40:57 -0700

    Working switch to overlay

--- overlay.nix
+++ overlay.nix
@@ -1,12 +1,12 @@
-{ config, lib, pkgs, ... }:
-
-let
-  callPackage = pkgs.callPackage;
-
-in (final: prev: rec {
+(final: prev: let
+  callPackage = prev.callPackage;
+  fetchgit = prev.fetchgit;
+  localLispPackages = final.localLispPackages;
+  
+in {
   letsencrypt-ca = callPackage ./pkgs/letsencrypt-ca.nix { };
 
-  minecraft-current = pkgs.minecraft-server_1_17_1;
+  minecraft-current = final.minecraft-server_1_17_1;
 
   minecraft-server_1_17_1 = prev.minecraft-server.overrideAttrs
     (oldAttrs: rec {
@@ -20,23 +20,23 @@ in (final: prev: rec {
 
   postgresql_11_gssapi = prev.postgresql_11.overrideAttrs (oldAttrs: rec {
     configureFlags = oldAttrs.configureFlags ++ [ "--with-gssapi" ];
-    buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+    buildInputs = oldAttrs.buildInputs ++ [ prev.krb5 ];
   });
 
   postgresql_12_gssapi = prev.postgresql_12.overrideAttrs (oldAttrs: rec {
     configureFlags = oldAttrs.configureFlags ++ [ "--with-gssapi" ];
-    buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+    buildInputs = oldAttrs.buildInputs ++ [ prev.krb5 ];
   });
 
   opencv-java = prev.opencv.overrideAttrs (oldAttrs: rec {
     # buildInputs = oldAttrs.buildInputs ++ [ pkgs.ant ];
     pname = "opencv-java";
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.jdk11 pkgs.ant ];
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.jdk11 prev.ant ];
     cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DWITH_JAVA=ON" ];
   });
 
   localLispPackages = (callPackage ./pkgs/lisp { inherit localLispPackages; })
-                      // pkgs.lispPackages;
+                      // prev.lispPackages;
 
   hll2380dw-cups = callPackage ./pkgs/hll2380dw-cups.nix { };
 
@@ -56,23 +56,23 @@ in (final: prev: rec {
 
   vanilla-forum = callPackage ./vanilla-forum.nix { };
 
-  openttd-data = pkgs.fetchgit {
+  openttd-data = fetchgit {
     url = "https://git.fudo.org/fudo-public/openttd-data.git";
     rev = "5b7dd0ca9014e642e1f2d0aa3154b5da869911d3";
     sha256 = "061k0f0jgm5k81djslb172xk0wkis0m878izgisyj2qgg3wf1awh";
   };
 
-  textfiles = pkgs.fetchgit {
+  textfiles = fetchgit {
     url = "https://git.informis.land/informis/textfiles.git";
     rev = "278a90f7ce219e36e5de0a80b540e469a9bce912";
     sha256 = "06qns3ayc84mamdgn0jw652rvx60wy9km1vxm2361mzmx2zk89iw";
   };
 
-  clj2nix = pkgs.callPackage (pkgs.fetchgit {
+  clj2nix = callPackage (fetchgit {
     url = "https://github.com/hlolli/clj2nix.git";
     rev = "e6d09dd8c5cda68eb0534bd8501f2d5dcd7b2e95";
     sha256 = "0v0q6iglr0lx13j1snzd8mjxids1af1p2h7bkvmsyk2bfp36naqx";
   }) { };
 
-  flatpak = pkgs.callPackage ./pkgs/flatpak { };
+  flatpak = callPackage ./pkgs/flatpak { };
 })
