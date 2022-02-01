ca3bd180598dccd4761856003314e8f78f0f2a8d
niten <niten@fudo.org>
2022-02-01 10:33:53 -0800

    use-ssl is not a bool, it's a keyword

--- pkgs/lisp/backplane-dns.nix
+++ pkgs/lisp/backplane-dns.nix
@@ -9,8 +9,8 @@ pkgs.lispPackages.buildLispPackage {
 
   src = pkgs.fetchgit {
     url = "https://git.fudo.org/fudo-public/backplane-dns.git";
-    rev = "02a5db8875d3b16b1bf4f38a35a4b60f42db71ca";
-    sha256 = "1xnz7wlgx98mm2q6qcgi93nq5mvr0rrfbddhn9x2s2b72ag9qp01";
+    rev = "8847d37724acf98350e1139e5cbdc04ffd7ef7cf";
+    sha256 = "0qjawwdibbfc4mc7rphm7gjlddxyh1lh1x8yqqsj64276wsjsc8n";
     fetchSubmodules = false;
   };
 
