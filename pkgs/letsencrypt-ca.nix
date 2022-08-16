9360d4d1ab0bc818d78c7160572d61fc1459ba1c
niten <niten@fudo.org>
2022-08-16 09:31:19 -0700

    Update letsencrypt CA hash

--- pkgs/letsencrypt-ca.nix
+++ pkgs/letsencrypt-ca.nix
@@ -2,7 +2,8 @@
 
 let
   url = "https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem";
-  sha256 = "1y00zqcrczsrv4403785df2b7q2hz4cf5z1kwlc58pd1g4gifc3a";
+  sha256 = "13526i3sjfhp6qqz7p7zil6r6a8knbgkhi9985863p4y68j1rk4m";
+  # sha256 = "1y00zqcrczsrv4403785df2b7q2hz4cf5z1kwlc58pd1g4gifc3a";
 
 in stdenv.mkDerivation {
 
