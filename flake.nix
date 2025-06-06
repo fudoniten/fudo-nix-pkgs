cc0db514944a0e06ceb4aa549b88f6530846129d
niten <niten@fudo.org>
2025-06-06 14:39:47 -0700

    Switch to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fudo packages";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     google-photo-uploader-flake.url = "github:fudoniten/google-photo-uploader";
     helpers.url = "github:fudoniten/fudo-nix-helpers";
     unstableNixpkgs.url = "nixpkgs/nixos-unstable";
@@ -14,7 +14,7 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          permittedInsecurePackages = [ "openssl-1.1.1w" ];
+          # permittedInsecurePackages = [ "openssl-1.1.1w" ];
           config.allowUnfree = true;
         };
         unstable = import unstableNixpkgs {
