8a723309e8f10fc1ca8b6744964e243a1b74ca20
niten <niten@fudo.org>
2026-06-18 09:35:59 -0700

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fudo packages";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     google-photo-uploader-flake.url = "github:fudoniten/google-photo-uploader";
     helpers.url = "github:fudoniten/fudo-nix-helpers";
     unstableNixpkgs.url = "nixpkgs/nixos-unstable";
