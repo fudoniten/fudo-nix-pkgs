af61d3247ce4880c12b34df12b3750ed29f4dc3c
niten <niten@fudo.org>
2026-06-18 15:47:03 -0700

    Wait, no it's not. The problem must be elsewhere.

--- pkgs.nix
+++ pkgs.nix
@@ -19,7 +19,9 @@ in rec {
 
   minecraft-current = minecraft-server_26_2;
 
-  minecraft-server_26_2 = pkgs.minecraft-server.overrideAttrs (oldAttrs: {
+  minecraft-server_26_2 = (pkgs.minecraft-server.override {
+    jre_headless = pkgs.jdk25_headless;
+  }).overrideAttrs (oldAttrs: rec {
     version = "26.2";
     src = fetchurl {
       url =
